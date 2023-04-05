4897ff087a46382fcdc60fb4094fd1ac4fec003b
saircode <sairjsanchez@gmail.com>
2023-04-05 15:45:29 -0500

    Con los CDN de bootstrap le fue mejor al proyecto .. creo que es por incompatibilidad con vue 3 y vite

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-
-import "@/assets/bootstrap/css/bootstrap.min.css";
-import "@/assets/bootstrap/js/bootstrap.min.js";
-import "@/assets/bootstrap/js/bootstrap.bundle.min.js";
-
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
