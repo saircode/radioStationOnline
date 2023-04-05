c30874ccd2ab92edd5f456d672ca09e6058e9857
saircode <sairjsanchez@gmail.com>
2023-04-05 14:53:47 -0500

    Instalacion de bootstrap ok

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+
+import "@/assets/bootstrap/css/bootstrap.min.css";
+import "@/assets/bootstrap/js/bootstrap.min.js";
+import "@/assets/bootstrap/js/bootstrap.bundle.min.js";
+
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// import './assets/main.css'
 
 const app = createApp(App)
 
