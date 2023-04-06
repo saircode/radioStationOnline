efda79177bb4fb15f84791088a7de00df5c36f60
saircode <sairjsanchez@gmail.com>
2023-04-06 08:54:45 -0500

    Se agrega el slider de noticias ... estas ahora estan fijas en el codigo pero la idea es que luego vengan desde un API creado en laravel

---
+++ src/components/home/SliderNews.vue
@@ -0,0 +1,37 @@
+<script>
+import { ref } from 'vue'
+    export default {
+        name: 'SliderNew',
+        setup(){
+            let dataNews = ref([
+                {tag: 'noticia-1', title: 'Noticia 1' , body: 'Lorem ipsum dolor', img: '/assets/img/newsSliderDemo/1.svg'},
+                {tag: 'noticia-2', title: 'Noticia 2' , body: 'Lorem ipsum dolor', img: '/assets/img/newsSliderDemo/2.svg'},
+                {tag: 'noticia-3', title: 'Noticia 3' , body: 'Lorem ipsum dolor', img: '/assets/img/newsSliderDemo/3.svg'}
+            ]);
+
+            return {
+                dataNews
+            }
+        }
+    }
+</script>
+
+
+<template>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item" :class="{'active' : key===0}" v-for="(item , key) in dataNews" :key="key">
+          <img class="d-block w-100" :src="item.img" :alt="item.title">
+        </div>
+        
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+</template>
